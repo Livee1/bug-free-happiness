11b4943dd19e151a1e655c7174e7ae5126da7b3c
Жингарева Юлия Сергеевна <yuliyason525@gmail.com>
2023-08-29 00:28:10 +0700

    Расстояние между точками

--- Sandbox/src/main/java/Main.java
+++ Sandbox/src/main/java/Main.java
@@ -1,22 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Point p1 = new Point(2.0,3.0);//конструктор
-        Point p2 = new Point(4.0,2.0);
-        System.out.println(distance(p1, p2));
-
-        Point p3 = new Point(1.2, 4.6);
-        Point p4 = new Point(-0.2, 1.1);
-        System.out.println(distance(p3, p4));
-
+        Point p1 = new Point(3, 5);
+        Point p2 = new Point(3, 6);
+        System.out.println("Расстояние между точками равно " + p1.distance(p2));
     }
-
-    public static double distance(Point p1, Point p2) {
-        double x1 = p1.x;
-        double y1 = p1.y;
-        double x2 = p2.x;
-        double y2 = p2.y;
-        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-
-    }
-
 }
--- Sandbox/src/main/java/Point.java
+++ Sandbox/src/main/java/Point.java
@@ -19,11 +19,17 @@
  * пункте запускаемый класс примеры использования метода вместо ранее созданной функции.
  */
 public class Point {
-    double x;//поле класса
-    double y;
-    public Point(double x1, double y1) {
-        x = x1;
-        y = y1;
+    public double x;
+    public double y;//public double x2;
+//public double y2;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p2) {
+        return Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
     }
 }
 
